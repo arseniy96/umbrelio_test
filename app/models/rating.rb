210804c866b66d226d231afa99d5701e172e7c7a
arseniy96 <arsenzhar@gmail.com>
2018-03-05 19:14:03 +0300

    Add validates to Rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,7 @@
 class Rating < ApplicationRecord
   belongs_to :post
+
+  validates :vote, presence: true
+  validates_numericality_of :vote, less_than_or_equal_to: 5
+  validates_numericality_of :vote, greater_than_or_equal_to: 1
 end
