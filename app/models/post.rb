1cb17f22ec45d740588c7cd5fc9a9dde28416871
arseniy96 <arsenzhar@gmail.com>
2018-03-05 18:13:02 +0300

    Add validates and scope to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :ratings
+
+  validates :title, presence: true
+  validates :text, presence: true
+
+  scope :top, -> (n) {where("posts.id in (select post_id from ratings)").group('posts.id').joins(:ratings).order('AVG(ratings.vote) DESC').limit(n)}
 end
